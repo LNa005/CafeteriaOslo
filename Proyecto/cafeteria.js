d9786bf3859067d04061f53c86e1303fd0abdd0c
juanontheweb24 <juanontheweb1@gmail.com>
2026-03-06 00:08:46 -0500

    Estructura js para el botón de navegacion

--- Proyecto/cafeteria.js
+++ Proyecto/cafeteria.js
@@ -1,8 +1,25 @@
-//Boton para el menu
+//Enlace botón hamburguesa
 let botonMenu = "#enlaceMenu";
 
 //Boton del menu a desplegar
 let menuPorDesplegar = "#menu";
 
 //clase para desplegar el menu lentamente (sin punto)
-let despliegaMenu = "despliega-menu";
+let despliegaMenuSwitch = "despliega-menu";
+
+function nav(){
+    let boton = document.querySelector(botonMenu);
+    boton.addEventListener("click", despliegaMenu, false);
+}
+
+
+function despliegaMenu(e){
+    e.preventDefault();
+    var despliega = document.querySelector(menuPorDesplegar);
+    despliega.classList.toggle(despliegaMenuSwitch);
+}
+//Clase para saber que el js está activo
+document.querySelector("html").classList.add("js");
+
+//Activar la función para el botón
+nav();
